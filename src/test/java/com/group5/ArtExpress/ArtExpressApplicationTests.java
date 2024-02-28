4f823535bd8495f781b7ac7ded2854948041c050
ikjames5454 <ikennajames03@gmail.com>
2024-02-28 11:53:34 +0100

    add

---
+++ src/test/java/com/group5/ArtExpress/ArtExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.ArtExpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
